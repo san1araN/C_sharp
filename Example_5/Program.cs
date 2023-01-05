b4ed875aa9bd5043c387ffa4d98cc1db49fc050e
Dmitriy Laziuk <dmitriy.vasilyev98@gmail.com>
2023-01-05 21:47:40 +0300

    Add Example_5

---
+++ Example_5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "женя")
+{
+        Console.WriteLine("Ура, это же Евгеша");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
